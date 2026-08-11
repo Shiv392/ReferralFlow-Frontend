fb35769f15a1b3b293df83af95a9ba2bfa9bd536
Shiv Soni <sonishiv309@gmail.com>
2026-08-11 16:24:59 +0530

    login and home page done

--- frontend-app/src/app/features/authentication/login/login.ts
+++ frontend-app/src/app/features/authentication/login/login.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login implements OnInit, OnDestroy {
+
+  private fb = inject(FormBuilder);
+
+  public loginForm : FormGroup;
+
+  constructor(){
+    this.loginForm = this.fb.group({
+      email : ['', [Validators.required, Validators.email]],
+      password : ['', [Validators.required]]
+    })
+  }
+
+  ngOnInit(): void{
+
+  }
+
+  public onSubmit():void{
+    
+  }
+
+  ngOnDestroy(): void{
+    
+  }
+
+}
--- frontend-app/src/app/features/home/home.ts
+++ frontend-app/src/app/features/home/home.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Button } from '../../shared/components/button/button';
 import { AnimateOnScrollModule } from 'primeng/animateonscroll';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -13,4 +14,11 @@ import { AnimateOnScrollModule } from 'primeng/animateonscroll';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+
+  private router = inject(Router);
+
+  public redirectLogin() : void{
+    this.router.navigate(['/auth/login']);
+  }
+}
